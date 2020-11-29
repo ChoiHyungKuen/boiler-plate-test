1f4e0d7dd69cdda648700d229cecb1456a08860c
gudrms1592 <gudrms1592@naver.com>
2020-11-29 23:17:20 +0900

    비밀

--- index.js
+++ index.js
@@ -2,16 +2,33 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const bodyParser = require('body-parser')
+
+const { User } = require("./models/User")
+
+const config = require('./config/key')
+
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://hgchoi:1234@myproject.nhho5.mongodb.net/<dbname>?retryWrites=true&w=majority', {
+mongoose.connect(config.mongoURI, {
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: true
 }).then(() => console.log('MongoDB connected...!'))
   .catch(err=>console.log(err))
 
-
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello 바꿨어요!!!')
+})
+
+app.post('/register', (req, res) => {
+    const user = new User(req.body)
+
+    user.save((err, userInfo) => {
+        if(err) return res.json({ success: false, err})
+
+        return res.status(200).json({success:true})
+    })
 })
 
 app.listen(port, () => {
